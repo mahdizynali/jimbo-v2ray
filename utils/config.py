c84d8d07f7598ba46bfb4c4f4f8cce6b378e91fc
mahdizynali <104717705+mahdizynali@users.noreply.github.com>
2023-05-07 13:35:16 +0330

    make util file

---
+++ utils/config.py
@@ -0,0 +1,13 @@
+import os
+
+directory = os.path.dirname(os.path.realpath(__file__)) 
+
+with open (directory + "/ipv4.txt", "r") as file:
+    ips = [line.strip() for line in file.readlines()]
+    file.close()
+
+
+payload = {
+    'range_ips' : ips,
+    'port' : 443
+}
