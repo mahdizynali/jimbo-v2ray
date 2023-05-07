8d66488810c5bbb170d44c8a35df1a9634ff6311
mahdizynali <zeinali.mahdy@gmail.com>
2023-05-07 22:55:07 +0330

    update features

--- main.py
+++ main.py
@@ -3,39 +3,43 @@ import time
 from utils.config import *
 
 # range_ip = payload["range_ips"]
-range_ip = "104.17.240.0/20"
+range_ip = "203.28.8.0/24"
 final_range = range_ip.replace(range_ip[-4::1],"")
 live_ip = []
 
-def upload (ip):
+def upload (ip) -> None :
     session_up = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    packet = b"a" * 1000000 # 0.1 MB of data
-    session_up.connect((ip, payload["port"]))
-    t0 = time.time()
-    session_up.send(packet)
-    session_up.close()
-    print(time.time() - t0)
+    socket.setdefaulttimeout(1)
+    packet = b"a" * 100000 # 0.1 MB of data
+    try :
+        session_up.connect((ip, payload["port"]))
+        t0 = time.time()
+        session_up.send(packet)
+        session_up.close()
+        print(time.time() - t0)
+    except :
+        print(ip, " out")
 
-
-def download (ip):
+def download (ip) -> None :
     session_down = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try :
-        session_down.bind((ip, payload["port"]))
-        session_down.listen()
+        session_down.bind((ip, 34425))
+        print("here")
+        session_down.listen(5)
         conn, addr = session_down.accept()
-        session_down = 0
+        data = 0
         t0 = time.time()
         while True:
             data = conn.recv(256*1024)
-            session_down += len(data)
-            if session_down == 1000000:
+            data += len(data)
+            if not data: 
                 break
         print(time.time() - t0)
     except :
-        print(ip, " fault")
+        print(ip, " download fault")
         
 
-def ip_scanner(ip_address):
+def ip_scanner(ip_address) -> bool :
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     socket.setdefaulttimeout(1)
     res = sock.connect_ex((ip_address, payload["port"]))
@@ -43,13 +47,25 @@ def ip_scanner(ip_address):
         return True
     
 # for ip in range(payload["end_point"] + 1):
-for ip in range(3):
+for ip in range(10):
     ip_address = final_range + str(ip)
-    if (ip_scanner(ip_address)):
-           live_ip.append(ip_address)
-           print(ip_address, "is live")
-           upload(ip_address)
-           download(ip_address)
-           print("="*20)
+    # if (ip_scanner(ip_address)):
+    live_ip.append(ip_address)
+    print(ip_address)
+    upload(ip_address)
+    # download(ip_address)
+    print("="*20)
 
-print("done")
+# import socketserver
+# def get_free_port() -> int:
+#     """returns a free port
+
+#     Returns:
+#         int: free port
+#     """
+#     with socketserver.TCPServer(("203.28.8.0", 0), None) as s:
+#         free_port = s.server_address[1]
+#     return free_port
+
+# print(get_free_port())
+
