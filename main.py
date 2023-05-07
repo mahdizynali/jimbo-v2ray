2bb7e7cfce0e5bf63c7d0f898e9e6398d202f48b
mahdizynali <104717705+mahdizynali@users.noreply.github.com>
2023-05-07 13:19:38 +0330

    open ip list in main file

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-import socket, time
+import socket
+import os
+import time
 from datetime import datetime
 
+directory = os.path.dirname(os.path.realpath(__file__)) 
+
+with open (directory + "/ipv4.txt", "r") as file:
+    range_ips = [line.strip() for line in file.readlines()]
+    file.close()
