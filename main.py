9e2ff14ab2959be1af673212a58d466ca8088ccc
mahdizynali <zeinali.mahdy@gmail.com>
2023-07-05 23:53:49 +0330

    classification for scanner header

--- main.py
+++ main.py
@@ -1,23 +1,9 @@
-# Authored By Mahdi Zeinali
+# Authored By Mahdi Zeinali - 2023
 # github : github.com/mahdizynali
 
-from utils.config import *
-from utils.Scanner import *
+from utils.Scanner import scanner
 
-# range_ip = payload["range_ips"]
 range_ip = "203.28.8.0/24"
 final_range = range_ip.replace(range_ip[-4::1],"")
-aliveIp = []
 
-# for ip in range(payload["end_point"] + 1):
-for ip in range(10):
-    ip_address = final_range + str(ip)
-    if (ip_scanner(ip_address)):
-        aliveIp.append(ip_address)
-        print(ip_address)
-        upload(ip_address)
-        download_speed_test(1000000,timeout=3,ips=ip_address)
-    print("="*20)
-
-
-
+scan = scanner(final_range, port=443, epoch=10)
