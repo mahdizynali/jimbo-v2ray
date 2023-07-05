97bcaa0a4851f078fac9dbe395c27dc404cfafb6
mahdizynali <104717705+mahdizynali@users.noreply.github.com>
2023-07-06 00:31:36 +0330

    add color for terminal result #1

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 
 from utils.Scanner import scanner
 
-range_ip = "203.28.8.0/24"
+range_ip = "185.201.139.0/24"
 final_range = range_ip.replace(range_ip[-4::1],"")
 
 scan = scanner(final_range, port=443, epoch=10)
